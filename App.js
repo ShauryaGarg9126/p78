19f007275830f43b3614cf31b6bfc2c1da74dd21
ShauryaGarg9126 <71919446+ShauryaGarg9126@users.noreply.github.com>
2021-04-24 09:42:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignupLoginScreen from './screens/WelcomeScreen';
+
+export default class App extends Component{
+render(){
+  return(
+
+    <WelcomeScreen/>
+
+  )
+}
+  
+}
